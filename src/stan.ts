84c0acce40f0b9f4db9b62890341dd009e07e514
Thiago Guerrero <54722457+T-Guerrero@users.noreply.github.com>
2021-02-18 21:35:52 -0300

    Update stan.ts

--- src/stan.ts
+++ src/stan.ts
@@ -2,7 +2,7 @@ import { connect } from 'node-nats-streaming';
 
 const clusterID = process.env.CLUSTER_ID || 'broker';
 const clusterClientID = process.env.CLUSTER_CLIENT_ID || 'pings';
-const clusterURL = process.env.CLUSTER_URL || 'nats://localhost:4222';
+const clusterURL = process.env.CLUSTER_URL || 'nats://broker:4222';
 
 export const stan = connect(clusterID, clusterClientID, {
   url: clusterURL,
