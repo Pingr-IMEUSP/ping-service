1f32f1b303d5412b1b62c460cfed69c430668c0e
Thiago Guerrero <tgbalera@hotmail.com>
2021-02-23 17:06:26 -0300

    refactoring
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- src/nats-streaming/listeners.ts
+++ src/nats-streaming/listeners.ts
@@ -10,24 +10,18 @@ export function setupListeners(): void {
   const createdUser = stan.subscribe('USER_CREATED', replayAllOpts);
 
   //João Corno
-  createdPing.on(
-    'message',
-    async (msg: Message): Promise<void> => {
-      const ping: PingInterface = JSON.parse(msg.getData() as string);
+  createdPing.on('message', (msg: Message): void => {
+    const ping: PingInterface = JSON.parse(msg.getData() as string);
 
-      await Ping.all.push(ping);
-      console.log('[PING_CREATED]:', ping);
-    },
-  );
+    Ping.all.push(ping);
+    console.log('[PING_CREATED]:', ping);
+  });
 
   //Wander Corno
-  createdUser.on(
-    'message',
-    async (msg: Message): Promise<void> => {
-      const user: UserInterface = JSON.parse(msg.getData() as string);
+  createdUser.on('message', (msg: Message): void => {
+    const user: UserInterface = JSON.parse(msg.getData() as string);
 
-      await User.all.push(user);
-      console.log('[USER_CREATED]:', user);
-    },
-  );
+    User.all.push(user);
+    console.log('[USER_CREATED]:', user);
+  });
 }
